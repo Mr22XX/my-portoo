c955ef0154d802d9f2cb5695557a1a5765a54d7e
Mr22XX <rayhanma0032@gmail.com>
2026-01-17 15:47:40 +0700

    2nd commit

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/Mr22XX">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/rayhanmuhammadadha/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
